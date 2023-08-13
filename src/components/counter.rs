869c38208599995cad6a5265acbbc7dd62052e66
Wesley Ricardi <wesley14_leandro@hotmail.com>
2023-08-13 20:25:12 +0000

    formating using css

--- src/components/counter.rs
+++ src/components/counter.rs
@@ -4,16 +4,18 @@ use leptos::*;
 pub fn CounterComponent(cx: Scope, count: u8) -> impl IntoView {
     view! {
         cx,
-        <main>
-        <div id="counting">
-        {count}</div>
-        <button hx-post="/increase"
+        <main class="h-80 w-80 rounded-full border-solid border-2 text-purple-600 font-bold border-purple-600 flex flex-col justify-around items-center">
+        <h2 class="text-3xl" id="counting">
+        {count}</h2>
+        <div
+         class="text-xl cursor-pointer hover:text-purple-300"
+         hx-post="/increase"
             hx-trigger="click"
             hx-target="#counting"
             hx-swap="innerHTML"
         >
             "Click Me!"
-        </button>
+        </div>
         </main>
     }
 }
--- src/pages/index.rs
+++ src/pages/index.rs
@@ -1,22 +1,26 @@
-use leptos::*;
 use crate::components::counter::CounterComponent;
+use leptos::*;
 
-pub async fn page_index(count:u8) -> String {
-    leptos::ssr::render_to_string(move |cx| view! { cx,
-        <head>
-            <meta charset="utf-8"/>
-            <meta http-equiv="X-UA-Compatible" content="IE=edge"/>
-            <title>"Counter using htmx + actix + leptos"</title>
-            <meta name="viewport" content="width=device-width, initial-scale=1"/>
-            <link rel="stylesheet" type="text/css" media="screen" href="static/styles/output.css"/>
-            <script src="https://unpkg.com/htmx.org@1.9.4" integrity="sha384-zUfuhFKKZCbHTY6aRR46gxiqszMk5tcHjsVFxnUo8VMus4kHGVdIYVbOYYNlKmHV" 
-            crossorigin="anonymous"></script>
-        </head>
-        <body>
-            <h1 class="text-4xl font-bold underline">
-            "Counter"
-            </h1>
-            <CounterComponent count=count />
-        </body>
-      })
+pub async fn page_index(count: u8) -> String {
+    leptos::ssr::render_to_string(move |cx| {
+        view! { cx,
+          <head>
+              <meta charset="utf-8"/>
+              <meta http-equiv="X-UA-Compatible" content="IE=edge"/>
+              <title>"Counter using htmx + actix + leptos"</title>
+              <meta name="viewport" content="width=device-width, initial-scale=1"/>
+              <link rel="stylesheet" type="text/css" media="screen" href="static/styles/output.css"/>
+              <script src="https://unpkg.com/htmx.org@1.9.4" integrity="sha384-zUfuhFKKZCbHTY6aRR46gxiqszMk5tcHjsVFxnUo8VMus4kHGVdIYVbOYYNlKmHV"
+              crossorigin="anonymous"></script>
+          </head>
+          <body>
+              <div class="bg-slate-800 h-screen flex justify-around items-center flex-col">
+                  <h1 class="text-4xl text-slate-200">
+                  "Counter"
+                  </h1>
+                  <CounterComponent count=count />
+              </div>
+          </body>
+        }
+    })
 }
